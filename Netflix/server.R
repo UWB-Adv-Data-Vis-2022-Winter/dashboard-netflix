ab1af0bc3f98d1276d2b2cee196955911d5061bd
Bac26094 <andyc8@uw.edu>
2022-02-23 14:49:32 -0800

    Filtered the data table of each tab to thier respective type

--- Netflix/server.R
+++ Netflix/server.R
@@ -6,16 +6,28 @@ library("DT")
 shinyServer(function(input, output) {
   netflix <- netflix_titles3
   
+#  output$tv_shows_graph <- renderPlot({
+#    netflix %>% 
+#      filter(
+#        type == "Tv Show"
+#      )
+#  }) 
   
   output$tv_shows <- renderDT({
     netflix %>% 
-      select(title, genre, date_added, duration, rating)
+      filter(
+        type == "TV Show"
+      )  %>%
+      select(title, genre, date_added, duration, rating) 
       
   })
   
   output$movies <- renderDT({
     netflix %>% 
-      select(title, genre, date_added, duration, rating)
+      filter(
+        type == "Movie"
+      )  %>%
+      select(title, genre, date_added, duration, rating) 
   })
 
   
--- Netflix/ui.R
+++ Netflix/ui.R
@@ -1,11 +1,13 @@
 library(shiny)
 library("DT")
 
+
 navbarPage(
   "Netflix Tv Shows and Movies", 
   tabPanel(
     "Tv Show",
     fluidPage(
+     # renderPlot("tv_show_graph"), 
       DTOutput("tv_shows")
     )
   ),
