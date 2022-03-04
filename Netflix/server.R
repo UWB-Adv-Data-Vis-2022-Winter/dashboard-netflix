e6868b911fa1c03ac0e54466b479b45a54e04171
Caleb Trujillo <calebtru@uw.edu>
2022-03-04 10:59:44 -0800

    Attempting to create an index of shows to help with the filtering of listedin column to select only a subset of genres.

--- Netflix/server.R
+++ Netflix/server.R
@@ -4,58 +4,75 @@ library(lubridate)
 
 shinyServer(function(input, output, session) {
   
+  #netflix_titles3<- read_csv("processed_netflix.csv")
   netflix <- netflix_titles3
   
   content_list <- unique(netflix_titles3$type)
   
   # create new column for genre
   netflix_titles3$genre <- strsplit(netflix_titles3$listed_in, ", ")
+  netflix_titles3$genre <- names(netflix_titles3$show_id)
   
   # return unique genre values 
   genre_list <- unlist(netflix_titles3$genre) %>%
     unique()
-
   
-updateSelectInput(session,
-                  "select_content_type", choices = content_list)
+updateSelectInput(session, inputId = "select_content_type", choices = content_list)
 
-updateSelectInput(session,
-                  "select_genre_tv_show", choices = genre_list)
+updateSelectInput(inputId = "select_genre_tv_show", 
+                  choices = genre_list)
 
-observe(netflix_tv_show_graph_filtered <- netflix_titles3 %>% 
-          filter(type %in% input$select_content_type) %>%
-          subset(input$select_genre %in% genre) %>%
-          group_by(date_year) %>%
-          summarize(n = n()) %>% 
-          drop_na()) 
+reactive({selected_genre <- netflix_titles3%>%
+  select(genre) %>%
+    unlist(recursive = FALSE) %>% 
+    enframe() %>% 
+  mutate(name = netflix_titles3$show_id) %>%
+    unnest() %>%
+  filter(value %in% input$selected_genre_tv_show)
+  })
+
     
 # bar graph of tv shows count per year    
 output$result <- renderText({
-  paste("You chose", unlist(input$select_genre))
+  paste("You chose", input$select_genre_tv_show)
 })
 output$netflix_bar_graph <- renderPlot({
-    
-    netflix_tv_show_graph_filtered <- netflix_titles3 %>% 
-      filter(type %in% input$select_content_type) %>%
-      subset(input$select_genre %in% genre) %>%
-      group_by(date_year) %>%
-      summarize(n = n()) %>% 
-      drop_na()
-     
-    ggplot(netflix_tv_show_graph_filtered) + 
+  netflix_titles3 %>% 
+    filter(type %in% input$select_content_type) %>%
+    filter(show_id %in% selected_genre$name) %>%
+    group_by(date_year) %>%
+    summarize(n = n()) %>% 
+    drop_na() %>%
+    ggplot() + 
       geom_col(aes(x = date_year, y = n))  
-    
-    netflix %>% select( )
  
 })
   
 # Data table of the tv shows  
   output$content_table <- renderDT({
     netflix_titles3 %>% 
-      filter(type %in% input$select_content_type) %>%
-      subset(input$select_genre %in% genre) %>%
+       filter(type %in% input$select_content_type) %>%
+      filter(show_id %in% (selected_genre$name))
       select(title, genre, date_added, duration, rating) 
       
+  })
+  
+  output$shows <- renderDT({
+    
+    colnames(selected_genre) <- c("show_id", "singlegenre")
+    titles <- netflix_titles3 %>% 
+      #filter(type %in% input$select_content_type) %>%
+      filter(show_id %in% (selected_genre$show_id)) %>%
+    select(title, genre, date_added, duration, rating, show_id) 
+    
+    #selected_genre%>%
+    #  left_join(selected_genre, titles) %>% 
+     # unique()
+      #select(title, genre, date_added, duration, rating)
+    
+    #subset(show_id %in% selected_genre()$name) %>%
+    #select(title, genre, date_added, duration, rating) 
+    
   })
   
 })
--- Netflix/ui.R
+++ Netflix/ui.R
@@ -11,11 +11,10 @@ navbarPage(
       
       selectInput("select_content_type", label = h3(" Select Content"), 
                   choices = content_list, selected  = NULL), 
-     
-      
-      selectInput("select_genre", label = h3(" Select Genre"),
+      selectInput("select_genre_tv_show", label = h3(" Select Genre"),
       choices = genre_list, multiple = TRUE, selected  = "Dramas"),
       textOutput("result"),
+      DTOutput("shows"),
       plotOutput("netflix_bar_graph"),
       DTOutput("content_table")
 )
