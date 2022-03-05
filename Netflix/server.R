772615255cac99c79ce0f68568e5fb880736b5b0
Caleb Trujillo <calebtru@uw.edu>
2022-03-04 16:17:57 -0800

    Remaking input panel and attempting to make the select reactive so we can filter within inputs later.

--- Netflix/server.R
+++ Netflix/server.R
@@ -4,30 +4,49 @@ library(lubridate)
 library(tidyverse)
 
 shinyServer(function(input, output, session) {
-  
+#load data and lists  
   content_list <- unique(netflix_titles3$type)
   netflix_titles3$genre <- strsplit(netflix_titles3$listed_in, ", ")
-  netflix_titles3$genre <- names(netflix_titles3$show_id)
+  names(netflix_titles3$genre) <- netflix_titles3$show_id
 
   genre_list <- unlist(netflix_titles3$genre) %>%
     unique()
-  
-updateSelectInput(session, inputId = "select_content_type", choices = content_list)
 
-updateSelectInput(inputId = "select_genre_tv_show", 
-                  choices = genre_list)
-
-selected_genre <- reactive({netflix_titles3%>%
-  select(genre) %>%
-    unlist(recursive = FALSE) %>% 
-    enframe() %>% 
-  mutate(name = netflix_titles3$show_id) %>%
-    unnest() %>%
-  filter(value %in% input$selected_genre_tv_show)%>%
-    `colnames<-`(c("show_id", "singlegenre"))
+  output$genre <- renderUI({
+    selectInput("select_genre_tv_show", label = h3(" Select Genre"),
+                choices = genre_list, multiple = TRUE, selected  = "Dramas")
   })
-
-
+  output$type <- renderUI({
+    selectInput("select_content_type", label = h3(" Select Content"), 
+                            choices = content_list, selected  = NULL)
+  })
+  
+  # Filtered data
+  selected_genre <- reactive({
+    netflix_titles3%>%
+      select(genre) %>%
+      unlist(recursive = FALSE) %>% 
+      enframe() %>% 
+      mutate(name = netflix_titles3$show_id) %>%
+      unnest(value) %>%
+      filter(value %in% input$type)%>%
+      `colnames<-`(c("show_id", "singlegenre"))
+  })
+  
+  data_filtered <- reactive({
+    netflix_titles3 %>% 
+    filter(genre %in% selected_genre(),
+           type %in% type())
+  })
+  
+  # Get filters from inputs
+  # genre <- reactive({
+  #   if (is.null(input$genre)) unique(netflix_titles3$genre) else input$genre
+  # })
+  
+  # type <- reactive({
+  #   if (is.null(input$type)) unique(df$type) else input$type
+  # })
     
 # bar graph of tv shows count per year    
 output$result <- renderText({
@@ -35,32 +54,26 @@ output$result <- renderText({
 })
 
 output$netflix_bar_graph <- renderPlot({
-  netflix_titles3 %>% 
-    #filter(type %in% input$select_content_type) %>%
-    #filter(show_id %in% selected_genre$name) %>%
-    group_by(date_year) %>%
+  
+  # data_filtered() %>%
+  #   group_by(date_year) %>%
+  #   summarize(n = n()) %>% 
+  #   drop_na() %>%
+  selected_genre() %>% 
+    group_by(single_genre) %>%
     summarize(n = n()) %>% 
     drop_na() %>%
-    ggplot() + 
-      geom_col(aes(x = date_year, y = n))  
+     ggplot() + 
+       geom_col(aes(x = singlegenre, y = n))  
  
 })
   
 # Data table of the tv shows  
-  output$content_table <- renderDT({
-    netflix_titles3 %>% 
-      #filter(type %in% input$select_content_type) %>%
-      #filter(show_id %in% (selected_genre$name)) %>%
-      select(title, genre, date_added, duration, rating) 
-      
-  })
   
   output$shows <- renderDT({
-    
-    
     titles <- netflix_titles3 %>% 
-      #filter(type %in% input$select_content_type) %>%
-      #filter(show_id %in% (selected_genre$show_id)) %>%
+      filter(type %in% input$type) %>%
+      filter(genre %in% (input$genre)) %>%
     select(title, genre, date_added, duration, rating, show_id) 
     
     #selected_genre%>%
@@ -75,6 +88,8 @@ output$netflix_bar_graph <- renderPlot({
   
 })
   
+
+
 
 
 
--- Netflix/ui.R
+++ Netflix/ui.R
@@ -6,10 +6,10 @@ library(lubridate)
 fluidPage(
   titlePanel("Netflix Tv Shows and Movies"), 
   sidebarLayout(
-    sidebarPanel(selectInput("select_content_type", label = h3(" Select Content"), 
-              choices = content_list, selected  = NULL), 
-      selectInput("select_genre_tv_show", label = h3(" Select Genre"),
-      choices = genre_list, multiple = TRUE, selected  = "Dramas")),
+    sidebarPanel(
+      uiOutput("genre"),
+      uiOutput("type")
+      ),
     mainPanel(
       textOutput("result"),
       DTOutput("shows"),
