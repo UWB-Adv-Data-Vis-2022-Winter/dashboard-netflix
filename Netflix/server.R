7d0f835a2aa342034f8bbb8968426af0ee9886b4
Bac26094 <andyc8@uw.edu>
2022-03-06 21:17:47 -0800

    added DT library to show data table

--- Netflix/server.R
+++ Netflix/server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library("DT")
 library(lubridate)
 library(tidyverse)
 library(reactlog)
--- Netflix/ui.R
+++ Netflix/ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library("DT")
 library(lubridate, warn.conflicts = FALSE)
 
 
